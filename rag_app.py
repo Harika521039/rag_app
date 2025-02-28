22ba693fa326cc49ee36dfb5a49fd1116ee4e63b
Harika521039 <harikayarakaraju5@gmail.com>
2025-02-28 12:21:41 +0530

    Update rag_app.py

--- rag_app.py
+++ rag_app.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 from PyPDF2 import PdfReader
 
 # Configure Google Gemini API key
-genai.configure(api_key="  ")
+genai.configure(api_key="AIzaSyBZnlpanyhyGnfEyi4XNTCAj2ledAi_1PI")
 
 # Function to read the PDF file
 def read_pdf(file_path):
